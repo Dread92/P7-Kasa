54130b13e6f3002b6ef75f995f350e6f96f8de95
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-04 22:00:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+       <Navbar/>
+    </div>
+  )
+}
+
+export default App
