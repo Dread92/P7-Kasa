1839d3817d497f9e7bd0d2e88d13b4ef9167c388
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-14 20:46:04 +0300

    refactoring and scss animations

--- src/components/DescriptionContainer.jsx
+++ src/components/DescriptionContainer.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./DescriptionContainer.scss";
 
 
  export function DescriptionContainer(props){
+    const [isContentVisible, setIsContentVisible]= useState(false);
+    const displayContent = () => {
+        setIsContentVisible(!isContentVisible)
+    }
+
     return(
         <div className='description__block'>
         <p className='description__title'>
             <span>{props.title}</span>
-            <i className="fa-solid fa-chevron-up"></i>
+            <i className="fa-solid fa-chevron-up" onClick={displayContent}></i>
         </p>
-        <p className='description__content'>{props.content}               
-        </p>
+       {isContentVisible && <p className='description__content'>{props.content}               
+        </p>}
     </div>
     );
 };
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './Spinner.scss';
 
 export const Spinner = () => {
-  return <div className="spinner"></div>;
+  return <div className="spinner">
+    <img src="logokasa.png" alt="Loading Spinner" />
+  </div>;
 };
 
