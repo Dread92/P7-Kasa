bb26d98c12fa14fbe67a692ecb5a20f86e7e692f
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-18 01:39:30 +0300

    usestate import

--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import './ImageBanner.scss'
+import React, { useState } from 'react';
+import './ImageBanner.scss';
+
+function ImageBanner(props) {
+  const pictures = props.pictures;
+  const [selectedPicture, setSelectedPicture] = useState(0);
+
+  const getClassName = (i) => {
+    if (i === selectedPicture) return 'show';
+    return '';
+  };
 
 
- function ImageBanner(props) {
-  
+const moveToNext = () =>{
+  setSelectedPicture((selectedPicture + 1) % pictures.length);
+};
+
+const moveToPrevious = () =>{
+  const newSelectedPicture= selectedPicture -1 ;
+  if (newSelectedPicture< 0){
+    setSelectedPicture(pictures.length -1 )
+    return;
+  }
+
+
+  setSelectedPicture((selectedPicture - 1) % pictures.length);
+};
+
 
   return (
     <div className='banner__img'>
-    <img src={props.imageUrl} alt=""/>
-</div>
+     
+      <div className='image__container'>
+      {pictures.map((pic, i) => (
+        <img key={pic} src={pic} alt='' className={getClassName(i)} />
+      ))}
+      </div>
+      < button onClick={moveToPrevious}>Previous</button>
+      <button onClick={moveToNext}>Next</button>
+       
+    </div>
   );
 }
 
-export default ImageBanner
+export default ImageBanner;
