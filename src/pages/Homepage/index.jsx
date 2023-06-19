1418af08fbd9ed9ea836f1aac02b12516a21aea8
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-20 02:15:44 +0300

    refactoring

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Homepage.scss";
-import Banner from '../../layout/Banner';
+import Banner from '../../layout/Banner/index';
 import {ApartmentsGrid} from '../../components/ApartmentsGrid/index';
 
 
