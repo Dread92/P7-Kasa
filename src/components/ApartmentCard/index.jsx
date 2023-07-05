8543bcb30502d54644dce43416f5727043b725c5
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-07-05 22:08:41 +0300

    gradient overlay on apartment grid

--- src/components/ApartmentCard/index.jsx
+++ src/components/ApartmentCard/index.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import "./ApartmentCard.scss";
 import { Link } from "react-router-dom";
 
-// Composant représentant une carte d'appartement
 export function ApartmentCard(props) {
-
   const { id, imageApartments, title } = props;
 
   return (
     <Link
-      to="/apartments" // Lien de redirection vers la page des appartements
+      to="/apartments"
       state={{
-        apartmentId: id // Passage de l'identifiant de l'appartement en tant que paramètre de l'état
+        apartmentId: id
       }}
     >
       <div className='apartment'>
         <img src={imageApartments} alt={title} />
+        <div className='gradient-overlay'></div>
         <div className='apartment__title'>{title}</div>
       </div>
     </Link>
-  )
+  );
 }
+
